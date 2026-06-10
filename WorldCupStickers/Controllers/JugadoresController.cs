b47424c30ef997fd50a646d2c9f2d64681730182
tcmel <tcmelest@gmail.com>
2026-06-09 21:14:38 -0500

    Creacion de jugadores desde API, 15 paises, sin crud de jugadores y paises

--- WorldCupStickers/Controllers/JugadoresController.cs
+++ WorldCupStickers/Controllers/JugadoresController.cs
@@ -1,9 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WorldCupStickers.Data;
-using WorldCupStickers.Models;
 using WorldCupStickers.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WorldCupStickers.Controllers;
 
@@ -71,116 +70,4 @@ public class JugadoresController : Controller
         return View(jugador);
     }
 
-    // GET: Jugadores/Create
-    public async Task<IActionResult> Create()
-    {
-        await PoblarEquiposAsync();
-        return View();
-    }
-
-    // POST: Jugadores/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Nombre,Posicion,NumeroCamiseta,FechaNacimiento,EquipoId")] Jugador jugador)
-    {
-        if (ModelState.IsValid)
-        {
-            _context.Add(jugador);
-            await _context.SaveChangesAsync();
-            TempData["Success"] = $"Jugador \"{jugador.Nombre}\" creado correctamente.";
-            return RedirectToAction(nameof(Index));
-        }
-        await PoblarEquiposAsync(jugador.EquipoId);
-        return View(jugador);
-    }
-
-    // GET: Jugadores/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
-            return NotFound();
-
-        var jugador = await _context.Jugadores.FindAsync(id);
-        if (jugador == null)
-            return NotFound();
-
-        await PoblarEquiposAsync(jugador.EquipoId);
-        return View(jugador);
-    }
-
-    // POST: Jugadores/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Posicion,NumeroCamiseta,FechaNacimiento,EquipoId")] Jugador jugador)
-    {
-        if (id != jugador.Id)
-            return NotFound();
-
-        if (ModelState.IsValid)
-        {
-            try
-            {
-                _context.Update(jugador);
-                await _context.SaveChangesAsync();
-                TempData["Success"] = $"Jugador \"{jugador.Nombre}\" actualizado correctamente.";
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!await _context.Jugadores.AnyAsync(j => j.Id == jugador.Id))
-                    return NotFound();
-                throw;
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        await PoblarEquiposAsync(jugador.EquipoId);
-        return View(jugador);
-    }
-
-    // GET: Jugadores/Delete/5
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null)
-            return NotFound();
-
-        var jugador = await _context.Jugadores
-            .Include(j => j.Equipo)
-            .Include(j => j.Cromos)
-            .FirstOrDefaultAsync(j => j.Id == id);
-
-        if (jugador == null)
-            return NotFound();
-
-        return View(jugador);
-    }
-
-    // POST: Jugadores/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        var jugador = await _context.Jugadores
-            .Include(j => j.Cromos)
-            .FirstOrDefaultAsync(j => j.Id == id);
-
-        if (jugador == null)
-            return NotFound();
-
-        if (jugador.Cromos.Any())
-        {
-            TempData["Error"] = $"No se puede eliminar \"{jugador.Nombre}\" porque tiene cromos asociados.";
-            return RedirectToAction(nameof(Index));
-        }
-
-        _context.Jugadores.Remove(jugador);
-        await _context.SaveChangesAsync();
-        TempData["Success"] = $"Jugador \"{jugador.Nombre}\" eliminado correctamente.";
-        return RedirectToAction(nameof(Index));
-    }
-
-    private async Task PoblarEquiposAsync(int? seleccionado = null)
-    {
-        ViewBag.Equipos = new SelectList(
-            await _context.Equipos.OrderBy(e => e.Nombre).ToListAsync(),
-            "Id", "Nombre", seleccionado);
-    }
 }
